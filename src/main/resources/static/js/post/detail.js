5319d83452f1517e277763a751eb58f1e248d2b6
stuuuu <woasmsshfl@naver.com>
2022-05-09 11:14:29 +0900

    좋아요, 좋아요 취소 UI구현 1/2

--- src/main/resources/static/js/post/detail.js
+++ src/main/resources/static/js/post/detail.js
@@ -1,9 +1,35 @@
+// 좋아요
+function postLikeClick(id) {
+    let isLike = $(`#heart-${id}`).hasClass("my_fake_like");
+    if (isLike) {
+        postUnLike(id);
+    } else {
+        postLike(id);
+    }
+}
+
+function postLike(id) {
+    // fetch();
+    $(`#heart-${id}`).addClass("my_fake_like");
+    $(`#heart-${id}`).removeClass("my_fake_un_like");
+    $(`#heart-${id}`).removeClass("far");
+    $(`#heart-${id}`).addClass("fa-solid");
+}
+
+function postUnLike(id) {
+    // fetch();
+    $(`#heart-${id}`).addClass("my_fake_un_like");
+    $(`#heart-${id}`).removeClass("my_fake_like");
+    $(`#heart-${id}`).removeClass("fa-solid");
+    $(`#heart-${id}`).addClass("far");
+}
+
 // 게시글 삭제, 권한체크 후 삭제 /s/api/post/postId
 $("#btn-delete").click(() => {
     postDelete();
 });
 
-let postDelete = async () => {
+let postDelete = async() => {
 
     let postId = $("#postId").val();
     let pageOwnerId = $("#pageOwnerId").val();
